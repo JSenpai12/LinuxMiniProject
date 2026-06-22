a7718c311a288918530fb728b989616c1069f716
Jhon Mekier Bayan <icecrackers7@gmail.com>
2026-06-23 02:55:20 +0800

    Solution 9

--- mySolution.sh
+++ mySolution.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/secretGenerator.tar.gz
-tar -xvzf secretGenerator.tar.gz
-
-
-# go to correct folder (safe even if run elsewhere)
 cd src
 
 # clean / prepare environment
